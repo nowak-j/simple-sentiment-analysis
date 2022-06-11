a5c3e7d6470ce0eedc07690067c3581ce1177acc
Joanna Nowak <nowak_joanna@interia.pl>
2022-06-11 19:08:32 +0200

    create main function

--- main.py
+++ main.py
@@ -1,44 +1,6 @@
 import matplotlib.pyplot as plt
 
-# sprawdzić język?
-text = input("Please enter text in English: ").split()      
 
-# jakoś słowa pozytywne i negatywne pobrać 
-positive = open('positive.txt', 'r')
-positive_words = positive.read()
-positive.close()
-
-negative = open('negative.txt', 'r')
-negative_words = negative.read()
-negative.close()
-
-stopwords = open('stopwords.txt', 'r')
-stopwords_lines = stopwords.read()
-stopwords.close()
-
-result = 0
-
-for word in text:
-    if word in positive_words:
-        result += 1
-    if word in negative_words:
-        result -= 1
-
-if result > 0:
-    print("The opinion is positive")
-elif result == 0:
-    print("The opinion is neutral")
-else:
-    print("The opinion is negative")
-
-
-# możliwość zrobienia ponownej analizy po usunięciu stopwords
-
-without_stopwords = []
-
-for word in text:
-    if word not in stopwords_lines:
-        without_stopwords.append(word)
 
 # wyświetlić najczęstsze pozytywne/negatywne, procent pozytywnych/negatywnych, 
 # inne statystyki poprzedzone wykresami
@@ -55,4 +17,50 @@ for word in text:
 
 # możliwość pobierania i analizowania tekstów z sieci
 
-# repozytorium na github
+
+def main():
+    text = input("Please enter text in English: ").split()      
+    
+    positive_words_file = open('positive.txt', 'r')
+    positive_words = positive_words_file.read()
+    positive_words_file.close()
+
+    negative_words_file = open('negative.txt', 'r')
+    negative_words = negative_words_file.read()
+    negative_words_file.close()
+
+    stopwords_file = open('stopwords.txt', 'r')
+    stopwords = stopwords_file.read()
+    stopwords_file.close()
+
+    analyse(text, positive_words, negative_words)
+
+    # możliwość zrobienia ponownej analizy po usunięciu stopwords
+
+    without_stopwords = []
+
+    for word in text:
+        if word not in stopwords:
+            without_stopwords.append(word)
+
+
+def analyse(text, positive_words, negative_words):
+    result = 0
+    for word in text:
+        if word in positive_words:
+            result += 1
+        if word in negative_words:
+            result -= 1
+
+    if result > 0:
+        print("The opinion is positive")
+    elif result == 0:
+        print("The opinion is neutral")
+    else:
+        print("The opinion is negative")
+
+
+
+
+if __name__ == "__main__":
+    main()
